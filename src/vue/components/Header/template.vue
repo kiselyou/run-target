7740e8ed8c71a9d686ad3948f8e4903a305c11b2
Valery Kiseliou <v.a.kiselyov@gmail.com>
2018-11-22 00:03:04 +0300

    Updated components RunProcess, GeoRun, Header.

--- src/vue/components/Header/template.vue
+++ src/vue/components/Header/template.vue
@@ -1,9 +1,9 @@
 <div class="header">
   <div class="row">
-    <div class="col-8 col-sm-8 col-md-2 col-lg-2 col-xl-2">
+    <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
       <div class="header_logo my-3 px-3">Run-Target</div>
     </div>
-    <div class="col-4 col-sm-4 col-md-10 col-lg-10 col-xl-10 text-right">
+    <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 text-right">
       <div class="header_menu my-3 px-3">
         <i class="oi oi-menu"></i>
       </div>
